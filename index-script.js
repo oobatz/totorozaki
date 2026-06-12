03524d6c092254f2cbb9539edb036ab53c9666fa
oobatz <chiloogly4@gmail.com>
2026-06-12 21:41:06 +0500

    Delete index-script.js

--- index-script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".order-hitbox")?.addEventListener("click", () => {
-  window.location.href = "menu.html";
-});
-
-document.querySelectorAll(".food-icon").forEach((button) => {
-  button.addEventListener("click", () => {
-    window.location.href = button.dataset.target || "menu.html";
-  });
-});
